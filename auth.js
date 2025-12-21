aa1a19ed254337bfcaa7c38fe34042e937855e32
Ashley <alawrence22@ivytech.edu>
2025-12-21 18:27:44 -0500

    redirect fix

--- auth.js
+++ auth.js
@@ -53,9 +53,7 @@ loginForm.addEventListener("submit", (e) => {
 
     signInWithEmailAndPassword(auth, email, password).then(cred => {
         console.log(cred.user);
-        alert("Logged in successfully");
-        loginForm.reset();
-        window.location.href = "https://ashlaw1989.github.io/WebsiteDevelopmentProject/geocaches.html";
+        window.location.href = "/WebsiteDevelopmentProject/geocaches.html";
     })
     .catch((err) => {
         console.error(err.message);
