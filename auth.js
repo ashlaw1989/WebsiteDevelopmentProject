bf66e85c2ed36a9789bc381c0b8dce007ba200ba
Ashley <alawrence22@ivytech.edu>
2025-12-21 13:38:12 -0500

    added logout functionality and modal

--- auth.js
+++ auth.js
@@ -1,27 +1,44 @@
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
+import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-auth.js";
 import { auth } from "./firebase.js";
 
 // signup
 const signupForm = document.querySelector("#signup-form");
-signupForm.addEventListener("submit", (e) => {
+if (signupForm) {
+    signupForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        //get user info
+        const email = signupForm["signup-email"].value;
+        const password = signupForm["signup-password"].value;
+
+        // sign up the user
+        createUserWithEmailAndPassword(auth, email, password).then(cred => {
+                alert("Account created successfully!");
+                const modal = document.querySelector("#modal-signup");
+                M.Modal.getInstance(modal).close();
+                signupForm.reset();
+            })
+
+            .catch((err) => {
+                console.error(err.message);
+                alert("Unable to create account.")
+            });
+    });
+
+}
+
+// logout
+const logout = document.getElementById("logout-popup");
+const closeLogout = document.getElementById("close-logout");
+
+document.getElementById("logout").addEventListener("click", (e) => {
     e.preventDefault();
-
-    //get user info
-    const email = signupForm["signup-email"].value;
-    const password = signupForm["signup-password"].value;
-
-    // sign up the user
-    createUserWithEmailAndPassword(auth, email, password).then(cred => {
-            console.log(cred.user);
-            alert("Account created successfully!");
-            const modal = document.querySelector("#modal-signup");
-            M.Modal.getInstance(modal).close();
-            signupForm.reset();
-        })
-        .catch((err) => {
-            console.error(err.message);
-            alert("Unable to create account.")
-        });
-
+    signOut(auth);
+    logout.style.display = "flex";
 });
+
+
+closeLogout.addEventListener("click", () => {
+    logout.style.display = "none";
+})
